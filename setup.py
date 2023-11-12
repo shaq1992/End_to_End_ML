95ec36cf4dd0a026c986e167801b47e0d8b08fce
Shadman Qaisar <shadmanqaisar7@gmail.com>
2023-11-12 14:30:18 +0530

    Setting up Requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "END_to_END_ML",
+    version = "0.0.1",
+    author = "shadman",
+    author_email = "shadmanqaisar7@gmail.com",
+    packages=find_packages(),
+    install_requires = ['pandas','numpy','seaborn']
+)
