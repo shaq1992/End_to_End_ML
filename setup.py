4d58450d205bea0ab7c27c9c430a1982facdd3c0
Shadman Qaisar <shadmanqaisar7@gmail.com>
2023-11-25 14:36:25 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import find_packages, setup
+from typing import List
+
+
+HYPHEN_E_DOT = "-e ."
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the requiremnts
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements= [req.replace("/n","") for req in requirements if req != HYPHEN_E_DOT]
+    return requirements
 
 setup(
     name = "END_to_END_ML",
@@ -6,5 +19,5 @@ setup(
     author = "shadman",
     author_email = "shadmanqaisar7@gmail.com",
     packages=find_packages(),
-    install_requires = ['pandas','numpy','seaborn']
+    install_requires = get_requirements('requirements.txt')
 )
